753581da9a37e43d23c54e3dcc7ff7d025f1596c
Rohith <rohith.kamath.mijar@tuhh.de>
2025-05-10 13:33:38 +0200

    add dvc lock

--- src/clasifier_cnn/pipeline/stage01_data_ingestion.py
+++ src/clasifier_cnn/pipeline/stage01_data_ingestion.py
@@ -1,6 +1,7 @@
 
 from clasifier_cnn.components.data_ingestion import DataIngestion
 from clasifier_cnn.config.configuration import ConfigurationManager
+from clasifier_cnn import logger
 
 class DataIngwationPipeline:
     def __init__(self):
@@ -13,4 +14,17 @@ class DataIngwationPipeline:
         data_ingestion.download_zip_file()
         data_ingestion.unzip_file()
 
+if __name__ == '__main__':
+
+    STAGE_NAME = "First Stage"
+
+    try:
+        
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngwationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     
+    except Exception as e:
+        logger.exception(e)
+        raise e
--- src/clasifier_cnn/pipeline/stage_02_prepare_base_model.py
+++ src/clasifier_cnn/pipeline/stage_02_prepare_base_model.py
@@ -1,5 +1,6 @@
 from clasifier_cnn.config.configuration import ConfigurationManager
 from clasifier_cnn.components.prepare_base_model import PrepareBaseModel
+from clasifier_cnn import logger
 
 STAGE_NAME = "second stage"
 
@@ -13,3 +14,16 @@ class PrepareBaseModelPipeline:
         prepare_base_model = PrepareBaseModel(config = prepare_base_model_config)
         prepare_base_model.get_base_model()
         prepare_base_model.update_base_model()
+
+if __name__ == '__main__':
+
+    STAGE_NAME = "Second Stage"
+
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+    raise e
